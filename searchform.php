f44bae45493e47905ca060b0922442001031a46d
Andrzej Bolewski <Andrzej Bolewski>
2020-09-17 16:28:36 +0200

    no message

--- searchform.php
+++
@@ -1,6 +0,0 @@
-<form role="search" method="get" id="searchform" action="<?php echo home_url( '/' ); ?>">
-    <div>
-        <input type="text" value="" name="s" id="s" placeholder="szukaj na stronie..." />
-        <input type="submit" class="btn btn-default" id="searchsubmit" value="Szukaj" />
-    </div>
-</form>
