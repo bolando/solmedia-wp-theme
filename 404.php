9a79b1663848c2997e290b8d913be93c5cb22627
Andrzej Bolewski <Andrzej Bolewski>
2020-09-17 16:16:12 +0200

    start

---
+++ 404.php
@@ -0,0 +1,22 @@
+<?php get_header(); ?>
+
+<section id="main-container">
+    <div class="container">
+
+        <article id="post-0" class="post no-results not-found">
+            <header>
+                <h1 class="entry-title">404 Nie znaleziono strony</h2>
+            </header>
+            <p>Chyba nie ma takiej strony. Może łącze, którego użyłeś, zawiera błąd?</p>
+            <p>Jeśli chcesz, możesz spróbować znaleźć to, czego tu szukałeś?</p>
+
+            <?php get_search_form(); ?>
+        </article>
+        
+    </div> 
+    
+<?php get_sidebar(); ?>
+
+</section>
+
+<?php get_footer(); ?>
---
+++ home.php
@@ -0,0 +1,11 @@
+<?php get_header(); ?>
+
+<section id="oferta">
+	<div class="container">
+		
+		Strona home.php
+	</div>
+</section>
+<?php dynamic_sidebar( 'widget_nazwa' ); ?>
+
+<?php get_footer(); ?>
