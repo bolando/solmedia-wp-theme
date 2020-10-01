85fef2996bd4e1d80045dec62f2765e6cbc413d3
Andrzej Bolewski <Andrzej Bolewski>
2020-10-01 14:53:41 +0200

    tutor

--- functions.php
+++ functions.php
@@ -1,7 +1,7 @@
 <?php
 function my_scripts() {
 	wp_enqueue_style( 'theme-styles', get_stylesheet_directory_uri() . '/style.css', array(), filemtime( get_stylesheet_directory() . '/style.css' ) );
-    wp_enqueue_script( 'script-name', get_template_directory_uri() . '/js/custom.js', array(), '1.0.0', true );
+    wp_enqueue_script( 'script-name', get_template_directory_uri() . '/js/custom.js', array(), filemtime( get_stylesheet_directory() . '/js/custom.js' ), true );
 }
 add_action( 'wp_enqueue_scripts', 'my_scripts' );
 
