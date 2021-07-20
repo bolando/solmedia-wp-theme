af48151c01d10e5742350b6f14ddabd399135c7f
Andrzej Bolewski <Andrzej Bolewski>
2021-07-20 16:25:21 +0200

    sticky header

--- js/custom.js
+++ js/custom.js
@@ -1,10 +1,15 @@
 jQuery(function(){
-	jQuery(document).scroll(function() {
+	//sticky header class
+	function ustawHeader(){
 		var pozycja = jQuery(document).scrollTop();
 		if (pozycja > 200) {
 			jQuery('#header').addClass('smaller')
 		} else {
 			jQuery('#header').removeClass('smaller')
 		}
+	}
+	ustawHeader();
+	jQuery(document).scroll(function() {
+		ustawHeader()
 	})
 })
