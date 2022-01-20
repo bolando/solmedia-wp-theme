2f5fe571f96c647ce9fda3d774da3c0cab05b0d7
Andrzej - Solmedia.pl <biuro@solmedia.pl>
2022-01-20 19:30:56 +0100

    Update search.php

--- search.php
+++ search.php
@@ -4,7 +4,9 @@
 		<section>
 			<h3>Wyniki wyszukiwania: <span class="search-highlight"><?php echo get_search_query(); ?></span></h3>
 			<p>&nbsp;</p>
-			<ol>
+			<?php $paged = (get_query_var('paged')) ? get_query_var('paged') : 1;
+			$start = 1 + ($paged - 1) * 10;?>
+			<ol start="<?php echo $start;?>">
 			<?php if (have_posts()): while (have_posts()) : the_post(); ?>
 				<li><a href="<?php the_permalink(); ?>" title="<?php the_title_attribute(); ?>"><?php search_title_highlight(); ?></a></li>
 				<?php //search_content_highlight(); ?>
