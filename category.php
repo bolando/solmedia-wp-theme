c6d3f3bc84d95efb4a70b4ae276d5c71ba087ecd
Andrzej - Solmedia.pl <biuro@solmedia.pl>
2022-04-14 05:07:54 +0200

    wrap-info

--- category.php
+++ category.php
@@ -15,7 +15,7 @@
 								</a>
 							<?php endif; ?>
 						</div>
-						<div class="wrap_info">
+						<div class="wrap-info">
 							<a href="<?php the_permalink(); ?>"><?php the_title('<h3 class="title sc-font">','</h3>'); ?></a>
 							<div class="date"><?php echo get_the_date(); ?></div>
 							<div class="text"><?php echo get_the_excerpt(); ?> <a href="<?php the_permalink(); ?>" title="<?php the_title_attribute(); ?>">[+]</a></div>
