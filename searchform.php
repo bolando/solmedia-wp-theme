2400ce1e12b19f4a03337300162619a9bee7aa14
Andrzej - Solmedia.pl <biuro@solmedia.pl>
2023-07-31 10:19:02 +0200

    31.07.23

--- searchform.php
+++ searchform.php
@@ -1,6 +1,6 @@
 <form role="search" method="get" id="searchform" action="<?php echo home_url( '/' ); ?>">
-    <div>
+    <div class="flex">
         <input type="text" value="" name="s" id="s" placeholder="szukaj na stronie..." />
-        <input type="submit" class="btn btn-default" id="searchsubmit" value="Szukaj" />
+        <input type="submit" class="kc_button" id="searchsubmit" value="Szukaj" />
     </div>
 </form>
