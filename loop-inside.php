e8fd5d89af0783b4fc5a2e523ac8dc687495ec82
Andrzej - Solmedia.pl <biuro@solmedia.pl>
2023-10-18 12:49:18 +0200

    Update loop-inside.php

--- loop-inside.php
+++ loop-inside.php
@@ -8,7 +8,9 @@
 	</div>
 	<div class="wrap-info">
 		<a href="<?php the_permalink(); ?>"><h3><?php the_title(); ?></h3></a>
-		<div class="date"><?php echo get_the_date(); ?></div>
+		<?php if (get_post_type() == 'post'):?>
+			<div class="date"><?php echo get_the_date(); ?></div>
+		<?php endif;?>
 		<div class="text"><?php echo get_the_excerpt(); ?> <a href="<?php the_permalink(); ?>" title="<?php the_title_attribute(); ?>">[+]</a></div>
 	</div>
 </div>
