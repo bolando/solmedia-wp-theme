70a82428f505f30d29fb987cc1737bae0314e92d
Andrzej - Solmedia.pl <biuro@solmedia.pl>
2023-10-26 09:10:00 +0200

    Update h1.php

--- h1.php
+++ h1.php
@@ -1,7 +1,7 @@
-<section id="tlo" <?php if (has_post_thumbnail()):?>style="background-image: url('<?php the_post_thumbnail_url();?>');" <?php endif;?>>
+<section id="tlo" <?php if (is_singular() && has_post_thumbnail()):?>style="background-image: url('<?php the_post_thumbnail_url();?>');" <?php endif;?>>
 	<div class="container">
 		<div class="ramka">
-			<h1><?php if (is_category()) {echo single_cat_title();} elseif (is_archive()) {post_type_archive_title();} elseif (is_tag()) {echo single_tag_title();} else {the_title();}; ?></h1>
+			<h1><?php if (is_home()) {echo 'Aktualności';} elseif (is_search()) {echo 'Wyniki wyszukiwania';} elseif (is_category()) {echo single_cat_title();} elseif (is_archive()) {post_type_archive_title();} elseif (is_tag()) {echo single_tag_title();} else {the_title();}; ?></h1>
 			<section id="breadcrumbs">
 				<?php if (function_exists('yoast_breadcrumb')) {yoast_breadcrumb();}?>
 			</section>
