17a88917a7308d663109141f3a5a9d6742c639e2
Andrzej - Solmedia.pl <biuro@solmedia.pl>
2023-10-30 13:53:41 +0100

    Update single.php

--- single.php
+++ single.php
@@ -6,7 +6,9 @@
 			<div class="row">
 				<div class="col-md-9">
 					<article id="post-<?php the_ID(); ?>" <?php post_class(); ?>>
-						<div class="date"><?php echo get_the_date(); ?></div>
+						<?php if (get_post_type() == 'post'):?>
+							<div class="date"><?php echo get_the_date(); ?></div>
+						<?php endif;?>
 						<?php the_content(); ?>
 					</article>
 				</div>
