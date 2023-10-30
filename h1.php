a7d678b07cc3059a9740618e72fbe97f5c305d26
Andrzej - Solmedia.pl <biuro@solmedia.pl>
2023-10-30 10:09:09 +0100

    Update h1.php

--- h1.php
+++ h1.php
@@ -2,9 +2,9 @@
 	<div class="container">
 		<div class="ramka">
 			<h1><?php if (is_home()) {echo 'Aktualności';} elseif (is_search()) {echo 'Wyniki wyszukiwania';} elseif (is_category()) {echo single_cat_title();} elseif (is_archive()) {post_type_archive_title();} elseif (is_tag()) {echo single_tag_title();} else {the_title();}; ?></h1>
-			<section id="breadcrumbs">
+			<div id="breadcrumbs">
 				<?php if (function_exists('yoast_breadcrumb')) {yoast_breadcrumb();}?>
-			</section>
+			</div>
 		</div>
 	</div>
 </section>
