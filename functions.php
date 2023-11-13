f6763e9c6f18162f4e46ca99755eb7c2e1c6b37f
Andrzej - Solmedia.pl <biuro@solmedia.pl>
2023-11-13 10:48:04 +0100

    Update functions.php

--- functions.php
+++ functions.php
@@ -89,3 +89,23 @@ function alx_excerpt_more( $more ) {
     return '...';
 }
 add_filter( 'excerpt_more', 'alx_excerpt_more' );
+
+//disable author page
+add_action('template_redirect', 'my_custom_disable_author_page');
+
+function my_custom_disable_author_page() {
+    global $wp_query;
+
+    if ( is_author() ) {
+        wp_redirect(get_option('home'), 301); 
+        exit; 
+    }
+}
+//Remove Gutenberg Block Library CSS from loading on the frontend
+function smartwp_remove_wp_block_library_css(){
+ wp_dequeue_style( 'wp-block-library' );
+ wp_dequeue_style( 'wp-block-library-theme' );
+ wp_dequeue_style( 'wc-blocks-style' ); // Remove WooCommerce block CSS
+wp_deregister_style( 'dashicons' ); //dashicons
+} 
+add_action( 'wp_enqueue_scripts', 'smartwp_remove_wp_block_library_css', 100 );
