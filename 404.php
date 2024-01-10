9a97b8102c70be7de4f8802b45f9ec16fafd61c5
Andrzej - Solmedia.pl <biuro@solmedia.pl>
2024-01-10 09:41:48 +0100

    Update 404.php

--- 404.php
+++ 404.php
@@ -5,7 +5,7 @@
 
         <article id="post-0" class="post no-results not-found">
             <header>
-                <h1 class="entry-title">404 Nie znaleziono strony</h2>
+                <h1 class="entry-title">404 Nie znaleziono strony</h1>
             </header>
             <p>Chyba nie ma takiej strony. Może łącze, którego użyłeś, zawiera błąd?</p>
         </article>
