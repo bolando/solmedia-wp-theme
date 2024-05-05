de4f4e9211ad776d007b30c0379914f8e2eeaed2
Andrzej - Solmedia.pl <biuro@solmedia.pl>
2024-05-05 22:45:43 +0200

    -dashicons

--- functions.php
+++ functions.php
@@ -106,7 +106,6 @@ function smartwp_remove_wp_block_library_css(){
  wp_dequeue_style( 'wp-block-library' );
  wp_dequeue_style( 'wp-block-library-theme' );
  wp_dequeue_style( 'wc-blocks-style' ); // Remove WooCommerce block CSS
-wp_deregister_style( 'dashicons' ); //dashicons
 } 
 add_action( 'wp_enqueue_scripts', 'smartwp_remove_wp_block_library_css', 100 );
 //podświetlenie wyszukiwania
